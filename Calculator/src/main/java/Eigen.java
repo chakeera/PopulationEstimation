dccade85ce610d15f0cf3ab2345ed3791f2eceb7
Natthakan Euaumpon <natthakaneuaumpon@gmail.com>
2020-03-17 20:43:48 +0700

    changes

---
+++ Calculator/src/main/java/Eigen.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Eigen {
+    public List<Long> getEVals(double[][] m){
+        List<Long> ans = new ArrayList<Long>();
+        long x = 0;
+        //to add
+        return ans;
+    }
+}
