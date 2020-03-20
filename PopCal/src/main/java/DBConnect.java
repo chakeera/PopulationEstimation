1cf9e0ee5822f6059a7ae7219a4e162ff84291f8
Sirin Chankao <sirin.cha@student.mahidol.edu>
2020-03-20 19:20:54 +0700

    Added DBConnect class; tested with mySql

---
+++ PopCal/src/main/java/DBConnect.java
@@ -0,0 +1,35 @@
+import java.sql.*;
+
+public class DBConnect
+{
+    public String connectDB()
+    {
+
+        try{
+            String url = ""; // {dbUrl}/{schema}
+            String username = "";
+            String password = "";
+
+            Class.forName("org.postgresql.Driver");
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery(""); //Enter query
+            while (resultSet.next())
+            {
+                System.out.println(resultSet.getString(1));
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        DBConnect dbConnect = new DBConnect();
+        dbConnect.connectDB();
+    }
+}
