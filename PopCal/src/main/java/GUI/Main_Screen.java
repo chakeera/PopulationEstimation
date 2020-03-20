2bc5804b9292f43ad94f251ac21f8547425a4453
Sirin Chankao <sirin.cha@student.mahidol.edu>
2020-03-20 17:23:51 +0700

    Created base GUI main screen

---
+++ PopCal/src/main/java/GUI/Main_Screen.java
@@ -0,0 +1,26 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main_Screen {
+    public static void main(String args[]) {
+        JFrame frame = new JFrame("Home");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 200);
+
+        JPanel topPanel = new JPanel();
+        JLabel label = new JLabel("Main Screen");
+        topPanel.add(BorderLayout.CENTER, label);
+
+        JPanel centerPanel = new JPanel();
+        JButton button1 = new JButton("Get Year Population");
+        JButton button2 = new JButton("Estimate Region Population");
+        centerPanel.add(button1);
+        centerPanel.add(button2);
+
+        frame.getContentPane().add(BorderLayout.NORTH, topPanel);
+        frame.getContentPane().add(BorderLayout.CENTER, centerPanel);
+        frame.setVisible(true);
+    }
+}
