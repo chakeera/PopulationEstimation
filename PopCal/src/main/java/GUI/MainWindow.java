5e186503303b103706b13568494727f1c0f164f7
Sirin Chankao <sirin.cha@student.mahidol.edu>
2020-03-29 03:23:46 +0700

    Combined all GUI windows to one main window

--- PopCal/src/main/java/GUI/MainWindow.java
+++ PopCal/src/main/java/GUI/MainWindow.java
@@ -1,46 +1,115 @@
 package GUI;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.List;
 
 public class MainWindow {
+
     public static void main(String args[]) {
         int width = 500;
-        int height = 200;
+        int height = 500;
+        final queries queries = new queries();
 
         JFrame frame = new JFrame("Home");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(width, height);
         frame.setResizable(false);
 
-        JPanel topPanel = new JPanel();
-        JLabel label = new JLabel("Home");
-        topPanel.add(BorderLayout.CENTER, label);
-
         JPanel centerPanel = new JPanel();
-        JButton getPopButton = new JButton("Population in Thailand");
-        JButton estimatePopButton = new JButton("Estimation of Population");
-        centerPanel.add(getPopButton);
-        centerPanel.add(estimatePopButton);
 
-        frame.getContentPane().add(topPanel, BorderLayout.NORTH);
+        JPanel topPanel = new JPanel();
+        TitledBorder topTitle = BorderFactory.createTitledBorder("Population in Thailand");
+        topTitle.setTitleJustification(TitledBorder.CENTER);
+        topPanel.setBorder(topTitle);
+        topPanel.setPreferredSize(new Dimension(width-25, height/2));
+//
+        //Enter Year Panel
+        JPanel yearPanel = new JPanel();
+        String[] years = {"2013", "2014", "2015", "2016",
+                "2017", "2018", "2019", "2020"};
+        final JComboBox yearList = new JComboBox(years);
+        yearPanel.add(new JLabel("Choose a year: "));
+        yearPanel.add(yearList);
+//
+//        //Enter Region Panel
+        JPanel regionPanel = new JPanel();
+        String[] regions = {"Bangkok", "Central", "North-eastern", "Northern", "Southern"};
+        final JComboBox regionList = new JComboBox(regions);
+        regionPanel.add(new JLabel("Choose a region: "));
+        regionPanel.add(regionList);
+
+        JButton calculateButton = new JButton("Get Population");
+        JButton resetButton = new JButton("Reset");
+
+        final JLabel resultLabel = new JLabel();
+
+        JPanel resultPanelTop = new JPanel();
+        resultPanelTop.setBorder(BorderFactory.createTitledBorder("Result"));
+        resultPanelTop.setPreferredSize(new Dimension(width-50, (height/2)/2));
+        resultPanelTop.add(resultLabel);
+
+        topPanel.add(yearPanel);
+        topPanel.add(regionPanel);
+        topPanel.add(calculateButton);
+        topPanel.add(resetButton);
+        topPanel.add(resultPanelTop);
+
+        JPanel bottomPanel = new JPanel();
+        TitledBorder bottomTitle = BorderFactory.createTitledBorder("Population Estimation");
+        bottomTitle.setTitleJustification(TitledBorder.CENTER);
+        bottomPanel.setBorder(bottomTitle);
+        bottomPanel.setPreferredSize(new Dimension(width-25, height/4));
+
+        JButton estimatePopButton = new JButton("Estimate Population");
+        final JLabel resultLabelBottom = new JLabel();
+        bottomPanel.add(estimatePopButton);
+        bottomPanel.add(resultLabelBottom);
+
+        centerPanel.add(topPanel);
+        centerPanel.add(bottomPanel);
         frame.getContentPane().add(centerPanel, BorderLayout.CENTER);
+//        frame.getContentPane().add(topPanel, BorderLayout.NORTH);
+//        frame.getContentPane().add(bottomPanel, BorderLayout.SOUTH);
         frame.setVisible(true);
 
-        final CurrentPopulationWindow currentPopulationWindow = new CurrentPopulationWindow();
-        final EstimationWindow estimationWindow = new EstimationWindow();
-
-        getPopButton.addActionListener(new ActionListener() {
+        calculateButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                currentPopulationWindow.showWindow();
+                queries.setRegion(regionList.getSelectedItem().toString()); //store value into variable region
+                queries.setYear(yearList.getSelectedItem().toString()); //store value into variable year
+                List<String> results = queries.getPopulationRegion();
+                resultLabel.setText("The year " + yearList.getSelectedItem() + " at region " +
+                        regionList.getSelectedItem() + " has a population " + results.get(0));
+            }
+        });
+
+        resetButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                resultLabel.setText("");
+                yearList.setSelectedIndex(0);
+                regionList.setSelectedIndex(0);
             }
         });
 
         estimatePopButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                estimationWindow.showWindow();
+                double[] results = queries.getEstPop();
+                NumberFormat formatter = new DecimalFormat("#0.00");
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append("The estimated population for the following age range is:\n");
+                stringBuilder.append("0-7: " + formatter.format(results[0]) + "\n");
+                stringBuilder.append("8-14: " + formatter.format(results[1]) + "\n");
+                stringBuilder.append("15-21: " + formatter.format(results[2]) + "\n");
+                stringBuilder.append("22-28: " + formatter.format(results[3]) + "\n");
+                stringBuilder.append("29-35: " + formatter.format(results[4]) + "\n");
+                stringBuilder.append("40 and above: " + formatter.format(results[5]));
+
+                resultLabelBottom.setText(stringBuilder.toString());
             }
         });
     }
